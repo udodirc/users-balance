30539b88b747eb311e20f41b39e7611bacd4c9ee
Salmin Lev <prostowb123@gmail.com>
2023-08-31 00:48:39 +0600

    feat(migrations, models): Create transactions table migration. Create Transaction model with relations.

---
+++ app/Models/Transaction.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'amount',
+        'type',
+        'user_id',
+    ];
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+}
